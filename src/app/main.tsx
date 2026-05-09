20eb8363108519e7357ae1533b4c353d4c958aed
ValkinDED <sonyerik289@gmail.com>
2026-05-09 19:05:26 +0200

    Smart Nutrition

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,32 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
+import App from "../App";
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./app/store";
-import { LanguageProvider } from "./shared/language";
-import { initializeClientPersistence } from "./shared/lib/clientPersistence";
-import { registerServiceWorker } from "./shared/lib/registerServiceWorker";
-import { AppThemeProvider } from "./shared/theme/AppThemeProvider";
+import { persistor, store } from "./store";
+
+import { LanguageProvider } from "../shared/language";
+import { initializeClientPersistence } from "../shared/lib/clientPersistence";
+import { registerServiceWorker } from "../shared/lib/registerServiceWorker";
+import { AppThemeProvider } from "../shared/theme/AppThemeProvider";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-await initializeClientPersistence();
+const bootstrap = async () => {
+  await initializeClientPersistence();
 
-root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <AppThemeProvider>
-          <LanguageProvider>
-            <App />
-          </LanguageProvider>
-        </AppThemeProvider>
-      </PersistGate>
-    </Provider>
-  </StrictMode>
-);
+  root.render(
+    <StrictMode>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <AppThemeProvider>
+            <LanguageProvider>
+              <App />
+            </LanguageProvider>
+          </AppThemeProvider>
+        </PersistGate>
+      </Provider>
+    </StrictMode>
+  );
 
-registerServiceWorker();
+  registerServiceWorker();
+};
+
+bootstrap();
