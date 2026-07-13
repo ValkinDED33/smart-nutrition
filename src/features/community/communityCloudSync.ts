33697b8c7efce3043da434fc554a43830fd539b6
ValkinDED <sonyerik289@gmail.com>
2026-07-13 09:10:06 +0200

    Smart-Nutrition

--- src/features/community/communityCloudSync.ts
+++ src/features/community/communityCloudSync.ts
@@ -37,7 +37,7 @@ const assertCloudSaved = async (
   );
 };
 
-export const saveCommunityStateToCloud = async (
+const saveCommunityStateToCloud = async (
   dispatch: AppDispatch,
   nextCommunity: CommunityState
 ) => {
